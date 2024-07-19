3e46d4da1fc4a3becd7ff4cd788b6eb10d84f0f7
Montooyadav <kumarmontoo2507@gmail.com>
2024-07-19 13:24:15 +0530

    Add files via upload

--- yadav Project/script.js
+++ yadav Project/script.js
@@ -20,7 +20,30 @@ function bindData(articles) {
         if (!article.urlToImage) return;
 
         const cardClone = newsCardTemplate.content.cloneNode(true);
-        // fillDataInCard(cardClone, article); // Uncomment and implement this line to fill card data
+        fillDataInCard(cardClone, article); 
         cardsContainer.appendChild(cardClone);
     });
 }
+
+function fillDataInCard(cardClone,article){
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-Desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US",{timeZone:"Asia/jakarta"});
+
+    newsSource.innerHTML = `${article.source.name} • ${date}`;
+
+}
+
+
+
+
+
+
+
